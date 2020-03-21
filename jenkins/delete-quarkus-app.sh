711241c02b7cc650abe33c10cedeed81619ee19d
Shinya Abe <grappling.world@gmail.com>
2020-03-22 04:17:45 +0900

    modified jenkins-pipeline

--- jenkins/delete-quarkus-app.sh
+++ jenkins/delete-quarkus-app.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-oc delete route quarkus-app
-oc delete is ubi-minimal
-oc delete is quarkus-app
-oc delete bc quarkus-app
-oc delete bc quarkus-sample-pipeline
-oc delete dc quarkus-app
-oc delete svc quarkus-app
+oc delete route quarkus-app-jenkins
+oc delete is ubi-minimal-jenkins
+oc delete is quarkus-app-jenkins
+oc delete bc quarkus-app-jenkins
+oc delete bc quarkus-jenkins-pipeline
+oc delete dc quarkus-app-jenkins
+oc delete svc quarkus-app-jenkins
